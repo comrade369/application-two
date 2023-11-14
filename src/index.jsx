49d7e2a64dff1d5793e928cc00e6395f90a8af9d
comrade369 <saveallchildreninworld@gmail.com>
2023-11-14 14:19:43 +0530

    Events difference in ReactJS And HTML Pure Javascript

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,33 @@
 // Gateway file for all Components to index.html file.
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
+// import App from "./components/App";
+
+// EVENTS IN REACTJS.
+const EventApp = () => {
+    const welcome = () => {
+        return window.alert("Welcome to 30 Days of ReactJS");
+    }
+
+    return (
+        <button onClick={welcome}>click me</button>
+    )
+}
+
+// we cannot return false to prevent default behaviour in react like in html.
+class EventClassApp extends React.Component {
+    welcome = () => {
+        // return window.alert("Preventing default behaviour explicitly");
+        console.log("The link was clicked");
+        return window.alert("Preventing default behaviour of html elements explicitly");
+    }
+    render() {
+        return (
+            <a href="https://www.google.com" onClick={this.welcome}>click link</a>
+        )
+    }
+}
+
 
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
-rootElement.render(<App />);
+rootElement.render(<EventClassApp />);
