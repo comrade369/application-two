d86241b7126a82513747fe4deca64306e3f94a51
comrade369 <saveallchildreninworld@gmail.com>
2023-11-14 15:24:53 +0530

    Events Handling for some Mouse and Keyboard Events in ReactJS

--- src/index.jsx
+++ src/index.jsx
@@ -28,6 +28,94 @@ class EventClassApp extends React.Component {
     }
 }
 
+// Different Events Handling of Mouse and Keyboard.
+class EventHandlingOfMouseAndKeyboardEvents extends React.Component {
+    // creating state object.
+    state = {
+        message: "",
+        firstName: "",
+        key: "",
+    }
+
+    // onClick Event Handle Function.
+    handleOnClick = (e) => {
+        // e gives the event object.
+        console.log(e);
+        // changing state 
+        this.setState({
+            message: "The onClick event is clicked."
+        })
+    }
+
+    handleOnMouseOver = (e) => {
+        console.log(e);
+        this.setState({
+            message: "The onMouseOver event is activated."
+        })
+    }
+
+    handleOnCopy = (e) => {
+        console.log(e);
+        this.setState({
+            message: "The onCopy event is activated."
+        })
+    }
+
+    handleOnKeyUp = (e) => {
+        console.log(e);
+        this.setState({
+            message: `${e.target.value} has been pressed, the keycode is ` + e.charcode,
+        })
+    }
+
+    handleOnBlur = (e) => {
+        console.log(e);
+        this.setState({
+            message: "The onBlur event is blurred the input field.",
+        })
+    }
+
+    handleOnChange = (e) => {
+        console.log(e);
+        console.log(this.state.value);
+        this.setState({
+            firstName: e.target.value,
+            message: e.target.value,
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h1>Welcome to World of Events.</h1>
+
+                <h2>Which Event Activated: {this.state.message}</h2>
+
+                <button onClick={this.handleOnClick}>On Click Event</button>
+
+                <button onMouseOver={this.handleOnMouseOver}>Move Mouse On Me</button>
+
+                <p onCopy={this.handleOnCopy}>Check copyright permission by copying this text.</p>
+
+                <label>Test for on key press event.</label>
+                <input type="text" onKeyUp={this.handleOnKeyUp} />
+
+                <label htmlFor="">Test for on blur event</label>
+                <input type="text" onBlur={this.handleOnBlur} />
+
+                <form onSubmit={this.handleOnSubmit}>
+                    <div>
+                        <label htmlFor="firstName">
+                            first name:
+                        </label>
+                        <input type="text" name="firstName" onChange={this.handleOnChange} value={this.state.value} />
+                    </div>
+                    <input type="submit" value="submit" />
+                </form>
+            </div>
+        )
+    }
+}
+
 
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
-rootElement.render(<EventClassApp />);
+rootElement.render(<EventHandlingOfMouseAndKeyboardEvents />);
