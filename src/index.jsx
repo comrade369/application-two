c47273bfda0f167da16a504d82b05a19749eaff9
comrade369 <saveallchildreninworld@gmail.com>
2023-11-14 20:13:28 +0530

    Getting data from multiple input fields in ReactJS

--- src/index.jsx
+++ src/index.jsx
@@ -1,39 +1,76 @@
-// Gateway file for all Components to index.html file.
+// Gateway for all components in react to index.html
 import React, {Component} from "react";
 import ReactDOM from "react-dom/client";
 
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 
-// FORMS IN REACTJS.
-class FormsApp extends Component {
+class FormsWithSameFieldApp extends Component {
     // Declaring state.
-    // Initial state.
     state = {
         firstName: "",
+        lastName: "",
+        country: "",
+        title: "",
     }
 
-    // setting value of firstName in event obj target field value field using onChange event handler.
+    // Events Handling for forms.
     handleOnChange = (e) => {
-        this.setState({
-            firstName: e.target.value,
-        })
+        /*
+            We can get name and value like this e.target.name && e.target.value,
+            But we can destructer the name, value from e.target
+            const name = e.target.name,
+            const value = e.target.value,
+        */
+       const {name, value} = e.target;
+       
+       this.setState({
+        [name]: value,
+       })
+
     }
+
+    handleOnSubmit = (e) => {
+        /*
+            e.preventDefault()
+            Stops the default behaviour of the form
+            Specifically refresh the page.
+        */
+        e.preventDefault();
+        /*
+            This is the place where we connect backend api,
+            to send data to database.
+        */
+
+        console.log(this.state);
+    }
+
+
 
     render() {
-        /*
-            we inject this.state.firstName value inside input value attribute.
-        */
+        // Destructering the state to access state values.
+        const {firstName, lastName, country, title} = this.state;
+
         return (
             <div>
-                <label htmlFor="firstName">First Name:</label>
-                <input type="text" name="firstName" id="firstName" 
-                    placeholder="first name" value={this.state.firstName}
-                    onChange={this.handleOnChange} />
-                
-                <h1>Display what we write on input field: {this.state.firstName}</h1>
+                <form action="" onSubmit={this.handleOnSubmit}>
+                    <input type="text" name="firstName" id="" 
+                        placeholder="First Name" value={firstName}
+                        onChange={this.handleOnChange} />
+                    <input type="text" name="lastName" id=""
+                        placeholder="Last Name" value={lastName}
+                        onChange={this.handleOnChange} />
+                    <input type="text" name="country" id=""
+                        placeholder="Country" value={country}
+                        onChange={this.handleOnChange} />
+                    <input type="text" name="title" id=""
+                        placeholder="Title" value={title}
+                        onChange={this.handleOnChange} />
+
+                    <button>Submit</button>
+                </form>
             </div>
         )
     }
 }
 
-rootElement.render(<FormsApp />);
+rootElement.render(<FormsWithSameFieldApp />);
