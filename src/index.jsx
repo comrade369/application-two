fdb3f5a434b5a900cad195b1027242fc5e54dbbb
comrade369 <saveallchildreninworld@gmail.com>
2023-11-15 10:04:46 +0530

    Get data from different input field types from Forms in ReactJS

--- src/index.jsx
+++ src/index.jsx
@@ -1,76 +1,218 @@
-// Gateway for all components in react to index.html
 import React, {Component} from "react";
 import ReactDOM from "react-dom/client";
 
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 
-class FormsWithSameFieldApp extends Component {
+// creating array of objects for select && option tags.
+const options = [
+    {value: "", label: "---Select Country---"},
+    {value: "india", label: "India"},
+    {value: "japan", label: "Japan"},
+    {value: "russia", label: "Russia"},
+    {value: "south korea", label: "South Korea"},
+    {value: "germany", label: "Germany"},
+];
+
+// Mapping the options to list(array) of JSX options.
+const selectOptions = options.map(({value, label}) => (
+    <option value={value}>{label}</option>
+));
+
+class FormsAppWithDifferentFieldTypes extends Component {
     // Declaring state.
     state = {
         firstName: "",
         lastName: "",
+        email: "",
+        telphone: "",
+        dateOfBirth: "",
+        favouriteColor: "",
+        weight: "",
+        gender: "",
+        bio: "",
+        file: "",
         country: "",
-        title: "",
+        skills: {
+            html: false,
+            css: false,
+            javascript: false,
+        },
+
     }
 
-    // Events Handling for forms.
+    // Handling Events 
     handleOnChange = (e) => {
-        /*
-            We can get name and value like this e.target.name && e.target.value,
-            But we can destructer the name, value from e.target
-            const name = e.target.name,
-            const value = e.target.value,
-        */
-       const {name, value} = e.target;
-       
-       this.setState({
-        [name]: value,
-       })
+        const {value, name, type, checked} = e.target;
 
+        if (type === "checkbox") {
+            this.setState({
+                skills: {...this.state.skills, [name]: checked}
+            })
+        } else if (type === "file") {
+            console.log(type, "click here");
+            this.setState({
+                [name]: e.target.files[0]
+            })
+        } else if (type === "radio") {
+            this.setState({
+                gender: value,
+            })
+        } else {
+            this.setState({
+                [name]: value,
+            })
+        }
+
+        console.log(e);
     }
 
     handleOnSubmit = (e) => {
-        /*
-            e.preventDefault()
-            Stops the default behaviour of the form
-            Specifically refresh the page.
-        */
         e.preventDefault();
-        /*
-            This is the place where we connect backend api,
-            to send data to database.
-        */
 
-        console.log(this.state);
+        const {firstName, lastName, email, telphone, dateOfBirth,
+            favouriteColor, weight, gender, bio, file, country, skills} = this.state;
+
+        const formattedSkills = [];
+
+        for (const key in skills) {
+            if (skills[key]) {
+                formattedSkills.push(key.toUpperCase())
+            }
+        }
+
+        const data = {firstName, lastName, email, telphone, dateOfBirth, favouriteColor,
+            weight, gender, bio, file, country, skills: formattedSkills,};
+        
+        console.log(data);
     }
-
-
-
+    
     render() {
-        // Destructering the state to access state values.
-        const {firstName, lastName, country, title} = this.state;
-
-        return (
+        
+        return(
             <div>
-                <form action="" onSubmit={this.handleOnSubmit}>
-                    <input type="text" name="firstName" id="" 
-                        placeholder="First Name" value={firstName}
-                        onChange={this.handleOnChange} />
-                    <input type="text" name="lastName" id=""
-                        placeholder="Last Name" value={lastName}
-                        onChange={this.handleOnChange} />
-                    <input type="text" name="country" id=""
-                        placeholder="Country" value={country}
-                        onChange={this.handleOnChange} />
-                    <input type="text" name="title" id=""
-                        placeholder="Title" value={title}
-                        onChange={this.handleOnChange} />
+                <h2>Add Student</h2>
 
-                    <button>Submit</button>
+                <form action="" onSubmit={this.handleOnSubmit} >
+                    <div>
+                        <div>
+                            <label htmlFor="firstName">First Name: </label>
+                            <input type="text" name="firstName" id="firstName"
+                                placeholder="First Name" value={this.state.firstName}
+                                onChange={this.handleOnChange} />
+                        </div>
+
+                        <div>
+                            <label htmlFor="lastName">Last Name: </label>
+                            <input type="text" name="lastName" id="lastName"
+                                placeholder="Last Name" value={this.state.lastName}
+                                onChange={this.handleOnChange} />
+                        </div>
+
+                        <div>
+                            <label htmlFor="email">E-Mail: </label>
+                            <input type="email" name="email" id="email"
+                                placeholder="E-Mail" value={this.state.email}
+                                onChange={this.handleOnChange} />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="telphone">Telphone: </label>
+                        <input type="tel" name="telphone" id="telphone"
+                            placeholder="Telphone" value={this.state.telphone}
+                            onChange={this.handleOnChange} />
+                    </div>
+
+                    <div>
+                        <label htmlFor="dateOfBirth">Date of Birth: </label>
+                        <input type="date" name="dateOfBirth" id="dateOfBirth"
+                            placeholder="Date of Birth" value={this.state.dateOfBirth}
+                            onChange={this.handleOnChange} />
+                    </div>
+
+                    <div>
+                        <label htmlFor="favouriteColor">Favourite Color: </label>
+                        <input type="color" name="favouriteColor" id="favouriteColor"
+                            placeholder="Favourite Color" value={this.state.favouriteColor}
+                            onChange={this.handleOnChange} />
+                    </div>
+
+                    <div>
+                        <label htmlFor="weight">Weight: </label>
+                        <input type="number" name="weight" id="weight"
+                            placeholder="Weight in kg" value={this.state.weight}
+                            onChange={this.handleOnChange} />
+                    </div>
+
+                    <div>
+                        <label htmlFor="country">Country</label>
+                        <br />
+                        <select name="country" id="country" onChange={this.handleOnChange}>
+                            {selectOptions}
+                        </select>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="male" id="male"
+                            value="Male" onChange={this.handleOnChange}
+                            checked={this.state.gender === "Male"} />
+                        <label htmlFor="male">Male</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="female" id="female"
+                            value="Female" onChange={this.handleOnChange}
+                            checked={this.state.gender === "Female"} />
+                        <label htmlFor="female">Female</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" name="other" id="other"
+                            value="Other" onChange={this.handleOnChange}
+                            checked={this.state.gender === "Other"} />
+                        <label htmlFor="other">Other</label>
+                    </div>
+
+                    <div>
+                        <p>Select your Skills:</p>
+                        <div>
+                            <input type="checkbox" name="html" id="html"
+                                onChange={this.handleOnChange} />
+                            <label htmlFor="html">HTML</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" name="css" id="css"
+                                onChange={this.handleOnChange} />
+                            <label htmlFor="css">CSS</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" name="javascript" id="javascript"
+                                onChange={this.handleOnChange} />
+                            <label htmlFor="javascript">Javascript</label>
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="bio">Bio</label>
+                        <textarea name="bio" id="bio" cols="120" rows="10"
+                            placeholder="Write about yourself..." value={this.state.bio}
+                            onChange={this.handleOnChange} />
+                    </div>
+
+                    <div>
+                        <input type="file" name="file" id="" onChange={this.handleOnChange}/>
+                    </div>
+
+                    <div>
+                        <button>Submit</button>
+                    </div>
+
                 </form>
             </div>
         )
     }
 }
 
-rootElement.render(<FormsWithSameFieldApp />);
+rootElement.render(<FormsAppWithDifferentFieldTypes />);
